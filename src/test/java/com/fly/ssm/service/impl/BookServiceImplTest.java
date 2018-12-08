aa73484856dbc9704cd0a99efb6b8f941cbfb7f1
XCZTH <1490493387@qq.com>
2018-12-08 23:31:05 +0800

    first commit

---
+++ src/test/java/com/fly/ssm/service/impl/BookServiceImplTest.java
@@ -0,0 +1,23 @@
+package com.fly.ssm.service.impl;
+
+
+import com.fly.ssm.BaseTest;
+import com.fly.ssm.dto.AppointExecution;
+import com.fly.ssm.service.BookService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BookServiceImplTest extends BaseTest {
+
+    @Autowired
+    private BookService bookService;
+
+    @Test
+    public void testAppoint() throws Exception {
+        long bookId = 1001;
+        long studentId = 12345678910L;
+        AppointExecution execution = bookService.appoint(bookId, studentId);
+        System.out.println(execution);
+    }
+
+}
